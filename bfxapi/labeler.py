82bfa8567774f648b2d821e78243d0cd8761f714
itsdeka <dario.moceri@bitfinex.com>
2023-01-10 12:44:19 +0100

    Handle submit order response (list of list)

--- bfxapi/labeler.py
+++ bfxapi/labeler.py
@@ -1,6 +1,6 @@
 from .exceptions import LabelerSerializerException
 
-from typing import Generic, TypeVar, Iterable, Optional, List, Tuple, Any, cast
+from typing import Generic, TypeVar, Iterable, Optional, List, Tuple, Any, cast, Dict, Union
 
 from types import SimpleNamespace
 
@@ -10,19 +10,39 @@ class _Serializer(Generic[T]):
     def __init__(self, name: str, labels: List[str], IGNORE: List[str] = [ "_PLACEHOLDER" ]):
         self.name, self.__labels, self.__IGNORE = name, labels, IGNORE
 
-    def _serialize(self, *args: Any, skip: Optional[List[str]] = None) -> Iterable[Tuple[str, Any]]:
+    def _serialize(self, *args: Any, skip: Optional[List[str]] = None) -> Union[Dict, List[Dict]]:
         labels = list(filter(lambda label: label not in (skip or list()), self.__labels))
 
-        # When an order is submitted args is a 1-element list containing a list, in other cases args is a list of values
         if type(args[0]) == list:
-            args = list(args)[0]
+            response = []
 
-        if len(labels) > len(args):
-            raise LabelerSerializerException("<labels> and <*args> arguments should contain the same amount of elements.")
+            for sub_args in args:
+                sub_response = {}
 
-        for index, label in enumerate(labels):
-            if label not in self.__IGNORE:
-                yield label, args[index]
+                if len(labels) > len(sub_args):
+                    raise LabelerSerializerException("<labels> and <*args> arguments should contain the same amount of elements.")
+
+                for index, label in enumerate(labels):
+                    if label not in self.__IGNORE:
+                        sub_response[label] = sub_args[index]
+
+                response.append(sub_response)
+        else:
+            response = {}
+
+            if len(labels) > len(args):
+                raise LabelerSerializerException("<labels> and <*args> arguments should contain the same amount of elements.")
+
+            for index, label in enumerate(labels):
+                if label not in self.__IGNORE:
+                    response[label] = args[index]
+
+        return response
 
     def parse(self, *values: Any, skip: Optional[List[str]] = None) -> T:
-        return cast(T, SimpleNamespace(**dict(self._serialize(*values, skip=skip))))
+        serialized_values = self._serialize(*values, skip=skip)
+
+        if type(serialized_values) == list:
+            return [ cast(T, SimpleNamespace(**value)) for value in serialized_values ]
+        else:
+            return cast(T, SimpleNamespace(**serialized_values))
--- examples/rest/create_order.py
+++ examples/rest/create_order.py
@@ -9,14 +9,19 @@ from bfxapi.utils.flags_helper import calculate_order_flags
 from examples.rest.authentication import *
 
 # Create a new order
-new_order = bfx.rest.auth.submit_order(symbol="tBTCUST", amount=0.015, price=10000, type=OrderType.EXCHANGE_LIMIT,
+new_orders_notification = bfx.rest.auth.submit_order(symbol="tBTCUST", amount=0.015, price=10000, type=OrderType.EXCHANGE_LIMIT,
                                        flags=calculate_order_flags(hidden=False))
+
+# The response is a list containing the orders submitted
+# In this case we have submitted only 1 order, so we can access it as follows
+
+new_order = new_orders_notification['NOTIFY_INFO'][0]
 print(new_order)
 
 # Update it
-updated_order = bfx.rest.auth.update_order(id=new_order["NOTIFY_INFO"].ID, price=10001)
+updated_order = bfx.rest.auth.update_order(id=new_order.ID, price=10001)
 print(updated_order)
 
 # Delete it
-deleted_order = bfx.rest.auth.cancel_order(id=updated_order["NOTIFY_INFO"].ID)
+deleted_order = bfx.rest.auth.cancel_order(id=new_order.ID)
 print(deleted_order)
